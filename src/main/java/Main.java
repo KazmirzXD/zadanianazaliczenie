86d253d6e67f23eb48998e43371d72519a8dba37
KazmirzXD <161707953+KazmirzXD@users.noreply.github.com>
2024-05-30 21:00:10 +0000

    1_15

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,32 +1,19 @@
-import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        Random random = new Random();
         Scanner scanner = new Scanner(System.in);
 
-        // Losowanie liczby od 0 do 100
-        int wylosowanaLiczba = random.nextInt(101); // nextInt(101) generuje liczby od 0 do 100
+        // Pobieranie łańcucha znaków od użytkownika
+        System.out.print("Podaj łańcuch znaków: ");
+        String lancuch = scanner.nextLine();
 
-        System.out.println("Zgadnij liczbę od 0 do 100:");
+        // Obliczanie długości łańcucha
+        int dlugosc = lancuch.length();
 
-        int zgadywanaLiczba = -1; // Inicjalizujemy zmienną wartością spoza zakresu, aby wejść do pętli
-
-        // Pętla do-while pozwalająca zgadywać, aż użytkownik poda poprawną liczbę
-        do {
-            System.out.print("Podaj swoją liczbę: ");
-            zgadywanaLiczba = scanner.nextInt();
-
-            if (zgadywanaLiczba < wylosowanaLiczba) {
-                System.out.println("Podana liczba jest za mała.");
-            } else if (zgadywanaLiczba > wylosowanaLiczba) {
-                System.out.println("Podana liczba jest za duża.");
-            } else {
-                System.out.println("Gratulacje! Zgadłeś liczbę.");
-            }
-        } while (zgadywanaLiczba != wylosowanaLiczba);
+        // Wyświetlanie długości łańcucha
+        System.out.println("Długość podanego łańcucha: " + dlugosc);
 
         scanner.close();
     }
