3444ccd9fc852d957b06fa26d8104cf757b782ec
KazmirzXD <161707953+KazmirzXD@users.noreply.github.com>
2024-05-30 20:57:18 +0000

    1_13

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,32 +1,38 @@
-import java.time.LocalDate;
-import java.time.Period;
 import java.util.Scanner;
 
 public class Main {
 
+    // Metoda sprawdzająca płeć na podstawie numeru PESEL
+    public static String okreslPlec(String pesel) {
+        // Sprawdzanie, czy PESEL ma długość 11 znaków
+        if (pesel.length() != 11) {
+            return "Nieprawidłowy numer PESEL.";
+        }
+
+        // Pobieranie 10-tego znaku z PESEL-u, który określa płeć
+        char znakPlec = pesel.charAt(9);
+
+        // Konwertowanie znaku na cyfrę
+        int cyfraPlec = Character.getNumericValue(znakPlec);
+
+        // Określanie płci na podstawie cyfry: parzysta - kobieta, nieparzysta - mężczyzna
+        if (cyfraPlec % 2 == 0) {
+            return "Płeć: Kobieta";
+        } else {
+            return "Płeć: Mężczyzna";
+        }
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Pobieranie daty urodzenia od użytkownika
-        System.out.print("Podaj rok urodzenia (YYYY): ");
-        int rok = scanner.nextInt();
+        // Pobieranie numeru PESEL od użytkownika
+        System.out.print("Podaj numer PESEL: ");
+        String pesel = scanner.nextLine();
 
-        System.out.print("Podaj miesiąc urodzenia (MM): ");
-        int miesiac = scanner.nextInt();
-
-        System.out.print("Podaj dzień urodzenia (DD): ");
-        int dzien = scanner.nextInt();
-
-        // Tworzenie obiektu LocalDate dla daty urodzenia
-        LocalDate dataUrodzenia = LocalDate.of(rok, miesiac, dzien);
-        // Tworzenie obiektu LocalDate dla bieżącej daty
-        LocalDate dzisiaj = LocalDate.now();
-
-        // Obliczanie wieku
-        Period wiek = Period.between(dataUrodzenia, dzisiaj);
-
-        // Wyświetlanie wyniku
-        System.out.println("Obecny wiek: " + wiek.getYears() + " lat");
+        // Określanie płci na podstawie PESEL-u
+        String plec = okreslPlec(pesel);
+        System.out.println(plec);
 
         scanner.close();
     }
