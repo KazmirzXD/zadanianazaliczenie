e8ddab7779e1dd6966fbb4163fd01efb2ac3b012
KazmirzXD <161707953+KazmirzXD@users.noreply.github.com>
2024-05-31 15:24:47 +0000

    1_30

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,25 +9,21 @@ public class Main {
         // Wyświetlanie oryginalnej tablicy
         System.out.println("Oryginalna tablica: " + Arrays.toString(tablica));
 
-        // Sortowanie tablicy w porządku malejącym
-        sortujTabliceMalejaco(tablica);
+        // Odwracanie tablicy
+        odwracajTablice(tablica);
 
-        // Wyświetlanie posortowanej tablicy
-        System.out.println("Tablica po sortowaniu malejącym: " + Arrays.toString(tablica));
+        // Wyświetlanie odwróconej tablicy
+        System.out.println("Tablica po odwróceniu: " + Arrays.toString(tablica));
     }
 
-    // Metoda do sortowania tablicy liczb całkowitych w porządku malejącym (sortowanie bąbelkowe)
-    public static void sortujTabliceMalejaco(int[] tablica) {
+    // Metoda do odwracania tablicy liczb całkowitych
+    public static void odwracajTablice(int[] tablica) {
         int n = tablica.length;
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = 0; j < n - 1 - i; j++) {
-                if (tablica[j] < tablica[j + 1]) {
-                    // Zamiana miejscami elementów
-                    int temp = tablica[j];
-                    tablica[j] = tablica[j + 1];
-                    tablica[j + 1] = temp;
-                }
-            }
+        for (int i = 0; i < n / 2; i++) {
+            // Zamiana miejscami elementów
+            int temp = tablica[i];
+            tablica[i] = tablica[n - 1 - i];
+            tablica[n - 1 - i] = temp;
         }
     }
 }
