dd1a4c2a040f97aae20269f12b4c49915a57f18e
KazmirzXD <161707953+KazmirzXD@users.noreply.github.com>
2024-05-31 15:22:41 +0000

    1_28

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,41 +1,23 @@
-import java.util.Scanner;
 import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        // Inicjalizacja tablicy znakowej
-        char[] tablica = {'a', 'b', 'c', 'd', 'e'};
+        // Inicjalizacja tablicy liczb całkowitych
+        int[] tablica = {5, 3, 8, 4, 2, 7, 1, 6};
 
         // Wyświetlanie oryginalnej tablicy
         System.out.println("Oryginalna tablica: " + Arrays.toString(tablica));
 
-        // Pobieranie indeksów od użytkownika
-        System.out.print("Podaj indeks pierwszego elementu do zamiany: ");
-        int indeks1 = scanner.nextInt();
-        System.out.print("Podaj indeks drugiego elementu do zamiany: ");
-        int indeks2 = scanner.nextInt();
+        // Sortowanie tablicy w porządku rosnącym
+        sortujTablice(tablica);
 
-        // Sprawdzanie, czy podane indeksy są poprawne
-        if (indeks1 >= 0 && indeks1 < tablica.length && indeks2 >= 0 && indeks2 < tablica.length) {
-            // Zamiana elementów w tablicy
-            zamienElementy(tablica, indeks1, indeks2);
-
-            // Wyświetlanie tablicy po zamianie elementów
-            System.out.println("Tablica po zamianie elementów: " + Arrays.toString(tablica));
-        } else {
-            System.out.println("Podane indeksy są niepoprawne.");
-        }
-
-        scanner.close();
+        // Wyświetlanie posortowanej tablicy
+        System.out.println("Tablica po sortowaniu: " + Arrays.toString(tablica));
     }
 
-    // Metoda do zamiany dwóch elementów w tablicy znakowej
-    public static void zamienElementy(char[] tablica, int indeks1, int indeks2) {
-        char temp = tablica[indeks1];
-        tablica[indeks1] = tablica[indeks2];
-        tablica[indeks2] = temp;
+    // Metoda do sortowania tablicy liczb całkowitych w porządku rosnącym
+    public static void sortujTablice(int[] tablica) {
+        Arrays.sort(tablica);
     }
 }
