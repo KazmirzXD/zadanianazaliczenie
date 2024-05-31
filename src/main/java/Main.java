4514573bac5fb45821176e98794f962ca6fe0fda
KazmirzXD <161707953+KazmirzXD@users.noreply.github.com>
2024-05-31 15:28:46 +0000

    1-32

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,39 +4,35 @@ import java.util.Set;
 public class Main {
 
     public static void main(String[] args) {
-        // Inicjalizacja dwóch tablic znakowych
-        char[] tablica1 = {'a', 'b', 'c', 'd', 'e'};
-        char[] tablica2 = {'c', 'd', 'e', 'f', 'g'};
+        // Inicjalizacja tablicy liczb całkowitych
+        int[] tablica = {1, 2, 3, 4, 5, 2, 6, 7, 8, 3, 9, 1};
 
-        // Wyświetlanie oryginalnych tablic
-        System.out.println("Tablica 1: " + new String(tablica1));
-        System.out.println("Tablica 2: " + new String(tablica2));
+        // Wyświetlanie oryginalnej tablicy
+        System.out.println("Oryginalna tablica: ");
+        for (int num : tablica) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
 
-        // Znajdowanie wspólnych elementów
-        Set<Character> wspolneElementy = znajdzWspolneElementy(tablica1, tablica2);
+        // Znajdowanie zduplikowanych wartości
+        Set<Integer> zduplikowaneWartosci = znajdzZduplikowaneWartosci(tablica);
 
-        // Wyświetlanie wspólnych elementów
-        System.out.println("Wspólne elementy: " + wspolneElementy);
+        // Wyświetlanie zduplikowanych wartości
+        System.out.println("Zduplikowane wartości: " + zduplikowaneWartosci);
     }
 
-    // Metoda do znajdowania wspólnych elementów między dwiema tablicami znakowymi
-    public static Set<Character> znajdzWspolneElementy(char[] tablica1, char[] tablica2) {
-        Set<Character> set1 = new HashSet<>();
-        Set<Character> set2 = new HashSet<>();
+    // Metoda do znajdowania zduplikowanych wartości w tablicy liczb całkowitych
+    public static Set<Integer> znajdzZduplikowaneWartosci(int[] tablica) {
+        Set<Integer> wszystkieWartosci = new HashSet<>();
+        Set<Integer> zduplikowaneWartosci = new HashSet<>();
 
-        // Dodawanie elementów pierwszej tablicy do zbioru
-        for (char c : tablica1) {
-            set1.add(c);
+        for (int num : tablica) {
+            // Jeśli wartość jest już w zbiorze wszystkieWartosci, dodaj ją do zbioru zduplikowaneWartosci
+            if (!wszystkieWartosci.add(num)) {
+                zduplikowaneWartosci.add(num);
+            }
         }
 
-        // Dodawanie elementów drugiej tablicy do zbioru
-        for (char c : tablica2) {
-            set2.add(c);
-        }
-
-        // Zachowywanie tylko wspólnych elementów
-        set1.retainAll(set2);
-
-        return set1;
+        return zduplikowaneWartosci;
     }
 }
