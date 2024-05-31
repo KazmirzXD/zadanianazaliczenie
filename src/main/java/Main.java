ed5fa0f3b13d4b401c04452fe2e18815aa18ca0b
KazmirzXD <161707953+KazmirzXD@users.noreply.github.com>
2024-05-31 15:24:00 +0000

    1_29

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,15 +9,25 @@ public class Main {
         // Wyświetlanie oryginalnej tablicy
         System.out.println("Oryginalna tablica: " + Arrays.toString(tablica));
 
-        // Sortowanie tablicy w porządku rosnącym
-        sortujTablice(tablica);
+        // Sortowanie tablicy w porządku malejącym
+        sortujTabliceMalejaco(tablica);
 
         // Wyświetlanie posortowanej tablicy
-        System.out.println("Tablica po sortowaniu: " + Arrays.toString(tablica));
+        System.out.println("Tablica po sortowaniu malejącym: " + Arrays.toString(tablica));
     }
 
-    // Metoda do sortowania tablicy liczb całkowitych w porządku rosnącym
-    public static void sortujTablice(int[] tablica) {
-        Arrays.sort(tablica);
+    // Metoda do sortowania tablicy liczb całkowitych w porządku malejącym (sortowanie bąbelkowe)
+    public static void sortujTabliceMalejaco(int[] tablica) {
+        int n = tablica.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (tablica[j] < tablica[j + 1]) {
+                    // Zamiana miejscami elementów
+                    int temp = tablica[j];
+                    tablica[j] = tablica[j + 1];
+                    tablica[j + 1] = temp;
+                }
+            }
+        }
     }
 }
