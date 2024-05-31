e3ad95ed2dff78e4b470c4556450c0f3adaf474c
KazmirzXD <161707953+KazmirzXD@users.noreply.github.com>
2024-05-31 15:27:08 +0000

    1-31

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,29 +1,42 @@
-import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Main {
 
     public static void main(String[] args) {
-        // Inicjalizacja tablicy liczb całkowitych
-        int[] tablica = {5, 3, 8, 4, 2, 7, 1, 6};
+        // Inicjalizacja dwóch tablic znakowych
+        char[] tablica1 = {'a', 'b', 'c', 'd', 'e'};
+        char[] tablica2 = {'c', 'd', 'e', 'f', 'g'};
 
-        // Wyświetlanie oryginalnej tablicy
-        System.out.println("Oryginalna tablica: " + Arrays.toString(tablica));
+        // Wyświetlanie oryginalnych tablic
+        System.out.println("Tablica 1: " + new String(tablica1));
+        System.out.println("Tablica 2: " + new String(tablica2));
 
-        // Odwracanie tablicy
-        odwracajTablice(tablica);
+        // Znajdowanie wspólnych elementów
+        Set<Character> wspolneElementy = znajdzWspolneElementy(tablica1, tablica2);
 
-        // Wyświetlanie odwróconej tablicy
-        System.out.println("Tablica po odwróceniu: " + Arrays.toString(tablica));
+        // Wyświetlanie wspólnych elementów
+        System.out.println("Wspólne elementy: " + wspolneElementy);
     }
 
-    // Metoda do odwracania tablicy liczb całkowitych
-    public static void odwracajTablice(int[] tablica) {
-        int n = tablica.length;
-        for (int i = 0; i < n / 2; i++) {
-            // Zamiana miejscami elementów
-            int temp = tablica[i];
-            tablica[i] = tablica[n - 1 - i];
-            tablica[n - 1 - i] = temp;
+    // Metoda do znajdowania wspólnych elementów między dwiema tablicami znakowymi
+    public static Set<Character> znajdzWspolneElementy(char[] tablica1, char[] tablica2) {
+        Set<Character> set1 = new HashSet<>();
+        Set<Character> set2 = new HashSet<>();
+
+        // Dodawanie elementów pierwszej tablicy do zbioru
+        for (char c : tablica1) {
+            set1.add(c);
         }
+
+        // Dodawanie elementów drugiej tablicy do zbioru
+        for (char c : tablica2) {
+            set2.add(c);
+        }
+
+        // Zachowywanie tylko wspólnych elementów
+        set1.retainAll(set2);
+
+        return set1;
     }
 }
